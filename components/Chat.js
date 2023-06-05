b7b3861be366bfc450324fd60270c52c50e9d13c
FabiMi <91958341+FabiMi@users.noreply.github.com>
2023-06-05 12:06:32 +0200

    Added AsyncStorage Logic

--- components/Chat.js
+++ components/Chat.js
@@ -1,15 +1,19 @@
-
 //Importing the necessary libraries and components
 import React, { useEffect, useState } from 'react';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 import { StyleSheet, View,  KeyboardAvoidingView,  Platform } from 'react-native';
 import { collection, addDoc, onSnapshot, query, orderBy, where } from 'firebase/firestore';
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 //Define the Chat component which will be the main component of the app.
-const Chat = ({ route, navigation, db }) => {
+const Chat = ({ route, navigation, db, isConnected }) => {
   const { userID, name, color, bubbleColor } = route.params;
   const [messages, setMessages] = useState([]);
+
+const loadCachedMessages = async () => {  
+  const cachedMessages = await AsyncStorage.getItem('messages');
+  SectionList(JSON.parse(cachedMessages));
+}
 
   // Define the addMessage function which will be used to add messages to the database.
   const addMessage = async (newMessages) => {
@@ -39,7 +43,7 @@ const Chat = ({ route, navigation, db }) => {
       console.error('Error sending message:', error);
     }
   };
-//
+// Define the useEffect hook to set the title of the screen and the header styles.
   useEffect(() => {
     navigation.setOptions({
       title: name,
@@ -47,8 +51,17 @@ const Chat = ({ route, navigation, db }) => {
       headerTintColor: bubbleColor,
     });
   }, []);
+// Define the useEffect hook to get the messages from the database.
 
-  useEffect(() => {
+
+let unsubMessages;
+
+useEffect(() => {
+
+if (isConnected ===true) {
+
+  if (unsubMessages) unsubMessages();
+  unsubMessages = null;
     const q = query(collection(db, 'messages'), orderBy('createdAt', 'desc'));
     const unsubMessages = onSnapshot(q, (docs) => {
       let newMessages = [];
@@ -59,13 +72,26 @@ const Chat = ({ route, navigation, db }) => {
           ...data,
           createdAt: data.createdAt ? new Date(data.createdAt.toMillis()) : new Date(),
         });
+        
       });
+      cacheMessages(newMessages);
       setMessages(newMessages);
     });
+  } else 
+    loadCachedMessages();
+
+    const cacheMessages = async (messageToCache) => {
+      try {
+        await AsyncStorage.setItem('messages', JSON.stringify(messageToCache));
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+
     return () => {
       if (unsubMessages) unsubMessages();
     };
-  }, []);
+  }, [isConnected]);
 
   const renderBubble = (props) => {
     return (
@@ -95,6 +121,8 @@ const Chat = ({ route, navigation, db }) => {
         onSend={(messages) => addMessage(messages)}
         user={{ _id: userID, name: name }}
         renderBubble={renderBubble}
+        renderUsernameOnMessage={true}
+
       />
       {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
     </View>
